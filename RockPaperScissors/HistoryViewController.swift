d01f1f3a2ce505cd1f4593430acb32e00ab61138
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-07-25 15:04:27 -0700

    Add required tableView datasource functions

--- RockPaperScissors/HistoryViewController.swift
+++ RockPaperScissors/HistoryViewController.swift
@@ -9,7 +9,22 @@
 import Foundation
 import UIKit
 
-class HistoryViewController: UIViewController {
+class HistoryViewController: UIViewController, UITableViewDataSource {
  
     var history = [RPSMatch]()
+    
+    // Data source methods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return history.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "historyCell")!
+        let results = self.history[(indexPath as NSIndexPath).row]
+        
+        cell.textLabel?.text = "Winner"
+        
+    return cell
+    }
 }
